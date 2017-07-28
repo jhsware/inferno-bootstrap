f309ae22d8418e096831ada980fbccdf962cff8b
jhsware <sebastian@urbantalk.se>
2017-07-28 16:07:06 +0200

    Moved compat code

--- lib/TetherContent.jsx
+++ lib/TetherContent.jsx
@@ -1,52 +1,13 @@
-import {
-  cloneVNode,
-  createVNode,
-  render
-} from "inferno";
+import { cloneVNode } from "inferno";
 import Component from 'inferno-component';
 import ReactDOM from 'react-dom';
 import isFunction from 'lodash.isfunction';
 import Tether from 'reactstrap-tether';
-
-/* COMPATIBILITY */
-// TODO: Rewrite implementation for Inferno
-
-function unmountComponentAtNode(container) {
-  render(null, container);
-  return true;
-}
-
-function unstable_renderSubtreeIntoContainer(
-  parentComponent,
-  vNode,
-  container,
-  callback
-) {
-  const wrapperVNode = createVNode(4, WrapperComponent, null, null, {
-    children: vNode,
-    context: parentComponent.context
-  });
-  const component = render(wrapperVNode, container);
-
-  if (callback) {
-    // callback gets the component as context, no other argument.
-    callback.call(component);
-  }
-  return component;
-}
-
-class WrapperComponent extends Component {
-  getChildContext() {
-    // tslint:disable-next-line
-    return this.props["context"];
-  }
-
-  render(props) {
-    return props.children;
-  }
-}
-
-/* /COMPATIBILITY */
+import {
+  unmountComponentAtNode,
+  unstable_renderSubtreeIntoContainer,
+  WrapperComponent
+} from './compat.js'
 
 const defaultProps = {
   isOpen: false,
