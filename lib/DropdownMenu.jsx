6ec0feed4bedf1d2d7f0f5c5235502968256f0b3
jhsware <sebastian@urbantalk.se>
2017-09-13 09:52:27 +0200

    USed the wrong code. However with the right code it stops working, need to investigate

--- lib/DropdownMenu.jsx
+++ lib/DropdownMenu.jsx
@@ -1,24 +1,40 @@
 import Inferno from 'inferno';
 import classNames from 'classnames';
 import { mapToCssModules } from './utils';
+import { Popper } from 'inferno-popper';
 
 const defaultProps = {
   tag: 'div',
+  flip: true,
 };
 
+const noFlipModifier = { flip: { enabled: false } };
+
 const DropdownMenu = (props, context) => {
-  const { className, cssModule, right, tag: Tag, ...attributes } = props;
+  const { className, cssModule, right, tag, flip, ...attrs } = props;
+  const position1 = context.dropup ? 'top' : 'bottom';
+  const position2 = right ? 'end' : 'start';
   const classes = mapToCssModules(classNames(
     className,
     'dropdown-menu',
-    { 
+    {
       'dropdown-menu-right': right,
-      'show': context.isOpen
+      show: context.isOpen,
     }
   ), cssModule);
 
+  attrs.placement = `${position1}-${position2}`;
+
   return (
-    <Tag {...attributes} tabIndex="-1" aria-hidden={!context.isOpen} role="menu" className={classes} />
+    <Popper
+      tabIndex="-1"
+      role="menu"
+      {...attrs}
+      component={tag}
+      aria-hidden={!context.isOpen}
+      className={classes}
+      modifiers={!flip ? noFlipModifier : undefined}
+    />
   );
 };
 
